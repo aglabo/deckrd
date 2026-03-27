b1d7758cf03c22f52fb0b99f4e057b25060e3667
Furukawa Atsushi <atsushifx@gmail.com>
2026-03-28 01:17:07 +0900

    test(deckrd/libs/tests/unit/session): align bootstrap usage with runtime libs
    - plugins/deckrd/skills/deckrd/scripts/libs/tests/unit/session.spec.sh:
      - replace `bootstrap.sh` with `bootstrap.lib.sh`
      - add "--no-finalize" flag to bootstrap invocation

--- plugins/deckrd/skills/deckrd/scripts/libs/tests/unit/session.spec.sh
+++ plugins/deckrd/skills/deckrd/scripts/libs/tests/unit/session.spec.sh
@@ -6,16 +6,16 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-_LIB_DIR="$(cd "${SHELLSPEC_PROJECT_ROOT}/plugins/deckrd/skills/deckrd/scripts/libs" && pwd)"
-# shellcheck disable=SC1091
-. "${_LIB_DIR}/bootstrap.sh"
-unset _LIB_DIR
+# shellcheck disable=SC1090
+# cspell:words myproject
+
+_RUNTIME_BOOTSTRAP="${SHELLSPEC_PROJECT_ROOT}/plugins/_runtime/libs/bootstrap.lib.sh"
+. "$_RUNTIME_BOOTSTRAP" "--no-finalize"
+unset _RUNTIME_BOOTSTRAP
 
 Include ../spec_helper.sh
 
 SCRIPT="${DECKRD_LIB_DIR}/session.sh"
-
-# shellcheck disable=SC1090
 . "$SCRIPT"
 
 # Schema used in tests
